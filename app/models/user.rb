ea0e5cd03551ab2eb5b1909b323f9149aa16b838
addo <forison6205@gmail.com>
2019-07-04 11:16:03 -0400

    create presence validation test and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username , presence: true
+    validates :country , presence: true
+    validates :email , presence: true 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: 'janet' ,email: 'janet@yahoo.com' ,country:'Ghana')
+    @valid_email= %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+  end
+
+  test "the truth" do
+    assert true if @user.valid?
+  end
+
+  test "check for the presence of values in username" do
+    assert_not @user.valid?  if @user.username = "   " 
+  end
+
+  test "check for the presence of values in email" do
+    assert_not @user.valid?  if @user.email = "   " 
+  end
+
+  test "check for the presence of values in country" do
+    assert_not @user.valid?  if @user.country = "   " 
+  end
+
 end
